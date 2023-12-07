df6ae90b87975b7658a6fd27f9a793cf75440c3a
RNaoki <Ricardo.N.Tanji@gmail.com>
2023-12-07 13:21:55 -0300

    Fix contact section + map

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -3,8 +3,8 @@ import Map from './Map';
 
 const ContactSection: React.FC = () => {
   return (
-    <div className="flex flex-row w-full bg-neutral-900 p-12 justify-between">
-      <div className="flex flex-col bg-neutral-800 h-[200px] w-[300px] justify-between p-4 rounded-md">
+    <div className="flex gap-12 flex-col lg:flex-row w-full bg-neutral-900 p-12 justify-between">
+      <div className="flex flex-col bg-neutral-800 h-[200px] w-[100%] lg:w-[25%] justify-between p-4 rounded-md">
         <Info label="Telefone" infoText="(12) 3456-7890" />
         <Info label="Endereço" infoText="Rua inexistente, 32" />
         <Info label="Email" infoText="email.falso@exemplo.com" />
